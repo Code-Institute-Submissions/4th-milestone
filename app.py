56f042dc8528caa4e94e7cb1b2ae334c9633363b
Aine Doyle <doy89@outlook.ie>
2018-12-20 21:31:39 +0000

    initial commit

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'onlinecookbook'
+app.config["MONGO_URI"] = 'mongodb://admin:Bettyboop234@ds139614.mlab.com:39614/onlinecookbook'
 
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return "Hello World...again"
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", 
+    recipes=mongo.db.recipes.find())
     
 
+
+
+# @app.route('/')
+# def hello():
+#     return "Hello World ...again"
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
