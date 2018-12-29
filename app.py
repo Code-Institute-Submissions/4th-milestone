d21defbd9dfaa4c824dff8f29f37c484e0738597
Aine Doyle <doy89@outlook.ie>
2018-12-29 22:25:17 +0000

    Added select element

--- app.py
+++ app.py
@@ -10,21 +10,24 @@ app.config["MONGO_URI"] = 'mongodb://admin:Bettyboop234@ds139614.mlab.com:39614/
 mongo = PyMongo(app)
 
 @app.route('/')
-@app.route('/get_recipe')
-def get_recipe():
+@app.route('/get_recipes')
+def get_recipes():
     return render_template("recipes.html", 
     recipes=mongo.db.recipes.find())
     
-@app.route('/add_recipe')
-def add_task():
-    return render_template('addrecipes.html')
     
-
-
-
-# @app.route('/')
-# def hello():
-#     return "Hello World ...again"
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('addrecipes.html',
+    cuisines=mongo.db.cuisines.find())
+    
+    
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes =  mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
+    
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
